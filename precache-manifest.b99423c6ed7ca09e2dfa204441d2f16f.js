e1d8f1691aa4e01d49809cbb117e8a743ce78ac6
ThaleSelaht <thalespizza@gmail.com>
2018-12-07 09:47:56 -0200

    Updates

---
+++ precache-manifest.b99423c6ed7ca09e2dfa204441d2f16f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d22494cd186bc3447bc890e3fbef9a",
+    "url": "/omie-prototype/static/media/funil-de-contas.87d22494.png"
+  },
+  {
+    "revision": "41253754825ec8c3465a",
+    "url": "/omie-prototype/static/js/runtime~main.41253754.js"
+  },
+  {
+    "revision": "05f86c7a8d743596f7d7",
+    "url": "/omie-prototype/static/js/main.05f86c7a.chunk.js"
+  },
+  {
+    "revision": "53c9b58888dacfd827c4",
+    "url": "/omie-prototype/static/js/1.53c9b588.chunk.js"
+  },
+  {
+    "revision": "05f86c7a8d743596f7d7",
+    "url": "/omie-prototype/static/css/main.7cfec847.chunk.css"
+  },
+  {
+    "revision": "53c9b58888dacfd827c4",
+    "url": "/omie-prototype/static/css/1.4e83c8fa.chunk.css"
+  },
+  {
+    "revision": "422fb9585a90609973ac05d9f6cd37be",
+    "url": "/omie-prototype/index.html"
+  }
+];
